f3002a3bdf787c3bc3ccb358248a20197f5798c0
BalajtiDani <Gyertya666@gmail.com>
2026-05-04 08:46:24 +0200

    Hazteteje

--- CsoportosHaz.py
+++ CsoportosHaz.py
@@ -5,7 +5,13 @@ i = 0
 while i < 4:
     turtle.forward(100)
     turtle.right(90)
-
-i += 1
+    i+=1
+i=0
+while i<1:
+    turtle.setheading(45)
+    turtle.forward(70)
+    turtle.setheading(-45)
+    turtle.forward(70)
+    i+=1
 
 turtle.done()
